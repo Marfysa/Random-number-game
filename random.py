b8aaccbde6f68f894de61e24cc29954a18fd51d3
Marfysa <marfysa14@gmail.com>
2023-09-11 21:13:43 +0300

    add validation random.py

--- random.py
+++ random.py
@@ -1,3 +1,12 @@
 import random
 num1 = random.randint(1,100)
 print("Добро пожаловать в числовую угадайку")
+def is_valid(n):
+    if n.isdigit:
+        if 1 <= int(n) <= 100:
+            return True
+        else:
+            return False
+
+n = input()
+print(is_valid(n))
